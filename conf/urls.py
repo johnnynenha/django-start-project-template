4f29fbdfdf0c8d630674df2489aa8b8c4e5f9d2e
Ján Madaj <madaj.jan@gmail.com>
2024-07-05 16:32:39 +0200

    initial commit

---
+++ conf/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from debug_toolbar.toolbar import debug_toolbar_urls
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += debug_toolbar_urls()
