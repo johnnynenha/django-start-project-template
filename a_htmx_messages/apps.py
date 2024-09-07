3d71541a332532e24911d19e2b9ba2e5828bb4fd
Ján Madaj <madaj.jan@gmail.com>
2024-09-07 21:35:36 +0200

    Add Messages + HTMX support

---
+++ a_htmx_messages/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AHtmxMessagesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'a_htmx_messages'
--- conf/settings.py
+++ conf/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib import messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
     "crispy_bootstrap5",
     "django_htmx",
     # apps
+    "a_htmx_messages",
     "a_home",
 ]
 
@@ -56,6 +58,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django_htmx.middleware.HtmxMiddleware",
+    "a_htmx_messages.middleware.HtmxMessagesMiddleware",
 ]
 
 ROOT_URLCONF = "conf.urls"
@@ -146,3 +149,14 @@ INTERNAL_IPS = [
     "127.0.0.1",
     # ...
 ]
+
+if DEBUG:
+    MESSAGE_LEVEL = messages.DEBUG
+
+MESSAGE_TAGS = {
+    messages.DEBUG: "text-bg-light",
+    messages.INFO: "text-bg-primary",
+    messages.SUCCESS: "text-bg-success",
+    messages.WARNING: "text-bg-warning",
+    messages.ERROR: "text-bg-danger",
+}
