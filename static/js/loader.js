9c590da33e93c784a8ee200d15c57eaa72f174c5
Ján Madaj <madaj.jan@gmail.com>
2024-09-07 18:55:37 +0200

    Fix Loader - remove after transition

--- static/js/loader.js
+++ static/js/loader.js
@@ -4,5 +4,9 @@
   window.addEventListener("load", () => {
     loader.classList.add("opacity-0");
     loader.classList.add("invisible");
+
+    loader.addEventListener("transitionend", () => {
+      document.body.removeChild(loader);
+    });
   });
 })();
