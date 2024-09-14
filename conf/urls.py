786845e6927e284e9266197e430402c4eae23377
Ján Madaj <madaj.jan@gmail.com>
2024-09-14 20:26:05 +0200

    Add static files in DEBUG

--- conf/urls.py
+++ conf/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("", include("a_home.urls")),
     path("admin/", admin.site.urls),
 ] + debug_toolbar_urls()
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
