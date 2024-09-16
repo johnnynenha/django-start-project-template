d5ca991518c8ed0faad1b5154a339f55b5d74749
Ján Madaj <madaj.jan@gmail.com>
2024-09-16 23:31:22 +0200

    A_users delete account

--- a_users/urls.py
+++ a_users/urls.py
@@ -6,4 +6,5 @@ from .views import *
 urlpatterns = [
     path("settings/", profile_settings, name="profile-settings"),
     path("email_verify/", profile_emailverify, name="profile-emailverify"),
+    path("delete/", profile_delete, name="profile-delete"),
 ]
--- a_users/views.py
+++ a_users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from allauth.account.utils import send_email_confirmation
 
 
@@ -12,3 +14,16 @@ def profile_settings(request):
 def profile_emailverify(request):
     send_email_confirmation(request, request.user)
     return redirect("profile-settings")
+
+
+@login_required
+def profile_delete(request):
+
+    if request.method == "POST":
+        user = request.user
+        auth_logout(request)
+        user.delete()
+        messages.success(request, "Account deleted successfully.")
+        return redirect("home")
+
+    return render(request, "a_users/profile_delete.html")
